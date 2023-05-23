f1ce3762c92cef29843e73101aad7a53d4c54507
epsilon <epsilonphenix@gmail.com>
2023-05-23 09:19:20 +0900

    Card

--- html/app/Http/Controllers/HomeController.php
+++ html/app/Http/Controllers/HomeController.php
@@ -4,26 +4,33 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use App\Models\Area;
+use App\Models\Genre;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $areas = Area::all();
+        $genres = Genre::all();
         if (Auth::check())
         {
             $user = User::IdEq(Auth::id())->first();
             if ($user->role_id == 1)
             {
+                $message=$user->name;
                 return view('Home.admin');
             }elseif ($user->role_id == 2)
             {
                 return view('Home.owner');
             }else
-                return view('Home.index');
+            $message=$user->name."さん、ようこそ";
+            return view('Home.index',compact('message'));
         }else
         {
-            return view('Home.index');
+            $message="ようこそ、Rese へ";
+            $areas =Area::all();
+            return view('Home.index',compact('message','areas','genres'));
         }
     }
 }
--- html/resources/views/Home/index.blade.php
+++ html/resources/views/Home/index.blade.php
@@ -7,7 +7,60 @@
 @section('pagetitle','Rese')
 
 @section('contents')
-    ユーザー画面
+    <div class="messageAndSearchbar layout__center-row">
+        <div class="message">
+            {{$message}}
+        </div>
+        <div class="searchbar">
+            <form method="post" class="layout__center-row" >
+                <div class="area_pulldown">
+                    <select name="area" id="">
+                        <option value="" selected>All Area</option>
+                        @foreach ($areas as $area)
+                            <option value="{{$area->id}}">
+                                {{$area->area}}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="genre_pulldown">
+                    <select name="genre" id="">
+                        <option value="" selected>All Genre</option>
+                        @foreach ($genres as $genre)
+                            <option value="{{$genre->id}}">
+                                {{$genre->genre}}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="search_text">
+                    <input type="text" name="search" id="search" placeholder="Search......">
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="Cards">
+        <div class="Card">
+            <div class="Card_img">
+                <img src="{{ asset('storage/images/1/sushi.jpg') }}">
+            </div>
+            <div class="Card_ShopName">
+                仙人
+            </div>
+            <div class="Tags">
+                東京都寿司
+            </div>
+            <div class="Buttons layout__center-row">
+                <button id='button' type="submit">
+                    詳しく見る
+                </button>
+                <div class="favorite">
+                    &#9829;
+                </div>
+            </div>
+        </div>
+    
+    </div>
     @if (count($errors) > 0)
         @foreach ($errors->all() as $error)
             {{$error}}
